1b0d1011a0a926a59c070822648cb6c17b27c9ae
unknown <alexander.evans@gmx.com>
2021-11-24 22:35:40 +0000

    First gui for meds in with all fields, no functionality

--- app/config.py
+++ app/config.py
@@ -8,5 +8,8 @@ SUGGEST_MANUAL = "Consider editing the medication in sheet manually after it has
 # ''' INPUT VALIDATION
 # These have not been set as CONST to allow easier implementation of user customisation later
 valid_freqs = ["i_n_per_day", "as_needed", "regime"]
+freqs = {"i_n_per_day": "Take [i], [n] times per day.",
+         "as_needed": "Take [i] as needed up to [n] times per day.",
+         "regime": "Take according to regime."}
 valid_strength_units = ["mg", "ml", None]
 valid_timings = ["morning", "night"]
--- app/gui.py
+++ app/gui.py
@@ -7,6 +7,20 @@ import sys
 import config as cfg
 
 
+class IntInput(QLineEdit):
+    def __init__(self, default_text=None, *args, **kwargs):
+        super(QLineEdit, self).__init__(*args, **kwargs)
+        self.setFixedWidth(25)
+        self.setPlaceholderText(default_text)
+        self.setAlignment(Qt.AlignHCenter)
+
+
+# class ReadOnlyTxt(QLineEdit): # gave up on this v quickly as existing QLabel is better
+#     def __init__(self, *args, **kwargs):
+#         super(QLineEdit, self).__init__(*args, **kwargs)
+#         self.isReadOnly = True
+
+
 class NewMedWindow(QWidget):
     def __init__(self, *args, **kwargs):
         super(QWidget, self).__init__(*args, **kwargs)
@@ -14,39 +28,70 @@ class NewMedWindow(QWidget):
         self.initUI()
 
     def initUI(self):
-        wrapper = QFrame()
-        grid = QGridLayout(wrapper)
-        self.setLayout(grid)
-
-        # set 3 column grid with 7 rows
-        # for r in range(7):
-        #     for c in range(3):
-        #         # grid.addWidget(QLabel(f"{r},{c}"), r, c)
-        #         grid.addWidget(QWidget())
+        # wrapper for entire form
+        wrapper = QVBoxLayout()
+        self.setLayout(wrapper)
 
         # medication name
+        row_med_name = QFormLayout()
         lbl_name = QLabel("Medication name")
         txt_name = QLineEdit()
-        grid.addWidget(lbl_name, 0, 0)
-        grid.addWidget(txt_name, 0, 1, 1, 3)
+        row_med_name.addRow(lbl_name, txt_name)
+        wrapper.addLayout(row_med_name)
 
         # medication strength
+        # quantity in
+        row_strength = QHBoxLayout()
         lbl_strength = QLabel("Strength")
-        txt_strength_val = QLineEdit()
-        grid.addWidget(lbl_strength, 1, 0)
-        grid.addWidget(txt_strength_val, 1, 1)
+        txt_strength = IntInput()
+        txt_strength.setMinimumWidth(100)
         cbo_strength_unit = QComboBox()
         for unit in cfg.valid_strength_units:
             cbo_strength_unit.addItem(unit)
-        grid.addWidget(cbo_strength_unit, 1, 2)
-
-        # dosage
-
-        # qty
+        cbo_strength_unit.setMaximumWidth(50)
         lbl_qty = QLabel("Qty In")
         txt_qty = QLineEdit()
-        grid.addWidget(lbl_qty, 3, 0)
-        grid.addWidget(txt_qty, 3, 1)
+        row_strength.addWidget(lbl_strength)
+        row_strength.addWidget(txt_strength)
+        row_strength.addWidget(cbo_strength_unit)
+        row_strength.addWidget(lbl_qty)
+        row_strength.addWidget(txt_qty)
+        wrapper.addLayout(row_strength)
+
+        # dosage
+        row_dosage = QHBoxLayout()
+        lbl_dosage = QLabel("Dosage")
+        cbo_dosage_type = QComboBox()
+        for type in cfg.freqs:
+            cbo_dosage_type.addItem(cfg.freqs[type])
+        txt_i = IntInput("i")
+        txt_n = IntInput("n")
+        row_dosage.addWidget(lbl_dosage)
+        row_dosage.addWidget(cbo_dosage_type)
+        row_dosage.addWidget(txt_i)
+        row_dosage.addWidget(txt_n)
+        wrapper.addLayout(row_dosage)
+
+        # statement
+        row_statement = QHBoxLayout()
+        dosage_statement = QLabel("...")
+        dosage_statement.setMinimumWidth(300)
+        dosage_statement.setAlignment(Qt.AlignHCenter)
+        btn_check = QPushButton("Check")
+        btn_check.setMaximumWidth(150)
+        row_statement.addWidget(dosage_statement)
+        row_statement.addWidget(btn_check)
+        wrapper.addLayout(row_statement)
+
+        # submit
+        row_buttons = QHBoxLayout()
+        btn_reset = QPushButton("Reset")
+        btn_submit = QPushButton("Submit")
+        row_buttons.addWidget(btn_reset)
+        row_buttons.addWidget(btn_submit)
+        wrapper.addLayout(row_buttons)
+
+        self.setLayout(wrapper)
 
 
 def main():
