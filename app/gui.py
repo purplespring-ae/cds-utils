71b9d868f574cde3808b87f1a7482211eb4bfd6a
unknown <alexander.evans@gmx.com>
2021-11-27 20:46:00 +0000

    Finished building out GUI for Meds In tool ready for socket/signal linking and functionality

--- app/gui.py
+++ app/gui.py
@@ -11,12 +11,29 @@ import config as cfg
 # WIDGET OVERRIDES
 
 
-class ShortIntInput(QLineEdit):
-    def __init__(self, default_text=None, *args, **kwargs):
+class TextInput(QLineEdit):
+    def __init__(self, min_width=None, max_width=None, default_text=None, *args, **kwargs):
         super(QLineEdit, self).__init__(*args, **kwargs)
-        self.setFixedWidth(25)
+        if min_width:
+            self.setMinimumWidth(min_width)
+        if max_width:
+            self.setMaximumWidth(max_width)
         self.setPlaceholderText(default_text)
+
+
+class DosageValueInput(QLineEdit):
+    def __init__(self, placeholder, name, *args, **kwargs):
+        super(QLineEdit, self).__init__(*args, **kwargs)
+        self.setPlaceholderText(placeholder)
+        self.setObjectName(name)
         self.setAlignment(Qt.AlignHCenter)
+
+
+class ComboBox(QComboBox):
+    def __init__(self, options=[], *args, **kwargs):
+        super(ComboBox, self).__init__(*args, **kwargs)
+        for opt in options:
+            self.addItem(opt)
 
 
 class HeadedTable(QTableWidget):
@@ -38,22 +55,174 @@ class TableColumnHeader(QTableWidgetItem):
 class MedsInTool(QWidget):
     def __init__(self, *args, **kwargs):
         super(QWidget, self).__init__(*args, **kwargs)
+        self.setWindowTitle("Medication In")
         self.build_UI()
 
     def build_UI(self):
-        def r_header():
+        def build_r_header():
             elem = QHBoxLayout()
-            lbl_title = QLabel("Medication In")
-            elem.addWidget(lbl_title)
+            # lbl_title = QLabel("Medication In")
+            # elem.addWidget(lbl_title)
             return elem
 
-        def t_med_editor():
+        def build_t_med_editor():
+            def r_med_name():
+                elem = QFormLayout()
+                lbl_name = QLabel("Medication name")
+                self.txt_name = QLineEdit()
+                elem.addRow(lbl_name, self.txt_name)
+                return elem
+
+            def r_str_qty():
+                elem = QHBoxLayout()
+                # -- strength input
+                lbl_strength = QLabel("Strength")
+                self.txt_strength = TextInput(100)
+                # -- strength select unit
+                self.cbo_strength_unit = ComboBox(cfg.valid_strength_units)
+                self.cbo_strength_unit.setMaximumWidth(50)
+                # -- qty
+                lbl_qty = QLabel("Qty In")
+                self.txt_qty = TextInput(None, 40)
+                # --- add to layout & return
+                elem.addWidget(lbl_strength)
+                elem.addWidget(self.txt_strength)
+                elem.addWidget(self.cbo_strength_unit)
+                elem.addWidget(lbl_qty)
+                elem.addWidget(self.txt_qty)
+                return elem
+
+            def r_input_dosage():
+                def opt1():
+                    # -- init option
+                    opt = QHBoxLayout()
+                    opt_str = "Take [i], [n] times per day."
+                    # -- widgets
+                    radio = QRadioButton("Take")
+                    radio.setStatusTip(opt_str)
+                    self.txt_i1 = DosageValueInput("i", "i1")
+                    self.txt_i1.setEnabled(False)
+                    lbl1 = QLabel(", ")
+                    self.txt_n1 = DosageValueInput("n", "n1")
+                    self.txt_n1.setEnabled(False)
+                    lbl2 = QLabel(" times per day.")
+                    # -- add to wlists
+                    radios.append(radio)
+                    self.dosage_inputs.extend([self.txt_i1, self.txt_n1])
+                    # -- add to layout& return
+                    opt.addWidget(radio)
+                    opt.addWidget(self.txt_i1)
+                    opt.addWidget(lbl1)
+                    opt.addWidget(self.txt_n1)
+                    opt.addWidget(lbl2)
+                    return opt
+
+                def opt2():
+                    # -- init option
+                    opt = QHBoxLayout()
+                    opt_str = "Take [i], up to [n] times per day."
+                    # -- widgets
+                    radio = QRadioButton("Take")
+                    radio.setStatusTip(opt_str)
+                    self.txt_i2 = DosageValueInput("i", "i2")
+                    self.txt_i2.setEnabled(False)
+                    lbl1 = QLabel(" as needed up to ")
+                    self.txt_n2 = DosageValueInput("n", "n2")
+                    self.txt_n2.setEnabled(False)
+                    lbl2 = QLabel(" times per day.")
+                    # -- add to wlists
+                    radios.append(radio)
+                    self.dosage_inputs.extend([self.txt_i2, self.txt_n2])
+                    # -- add to layout & return
+                    opt.addWidget(radio)
+                    opt.addWidget(self.txt_i2)
+                    opt.addWidget(lbl1)
+                    opt.addWidget(self.txt_n2)
+                    opt.addWidget(lbl2)
+                    return opt
+
+                def opt3():
+                    # -- init option
+                    opt = QHBoxLayout()
+                    opt_str = "Take according to regime."
+                    # -- widgets
+                    radio = QRadioButton(opt_str)
+                    radio.setStatusTip(opt_str)
+                    # -- add to layout, wlists & return
+                    radios.append(radio)
+                    opt.addWidget(radio)
+                    return opt
+
+                def opt4():
+                    # -- init option
+                    opt = QHBoxLayout()
+                    # -- widgets
+                    radio = QRadioButton("Custom")
+                    radio.setStatusTip("Custom: ")
+                    self.txt_custom_dosage = TextInput(
+                        100, None, "Caution - use presets where possible.")
+                    self.txt_custom_dosage.setEnabled(False)
+                    # -- add to wlists
+                    radios.append(radio)
+                    self.dosage_inputs.append(self.txt_custom_dosage)
+                    # -- add to layout & return
+                    opt.addWidget(radio)
+                    opt.addWidget(self.txt_custom_dosage)
+                    return opt
+
+                elem = QVBoxLayout()
+                lbl_dosage = QLabel("Dosage:")
+                radios = []
+                self.dosage_inputs = []
+                # --- add to element & return
+                elem.addWidget(lbl_dosage)
+                elem.addLayout(opt1())
+                elem.addLayout(opt2())
+                elem.addLayout(opt3())
+                elem.addLayout(opt4())
+                # --- radio buttons
+                self.dosage_options = QButtonGroup()
+                for radio in radios:
+                    self.dosage_options.addButton(radio)
+                # --- i, n textboxes
+                #
+                # --- return
+                return elem
+
+            def r_display_dosage():
+                # --- layout
+                elem = QHBoxLayout()
+                # --- widgets
+                self.dosage_statement = QLabel("...")
+                self.dosage_statement.setMinimumWidth(300)
+                self.dosage_statement.setAlignment(Qt.AlignHCenter)
+                self.btn_validate = QPushButton("Check")
+                self.btn_validate.setMaximumWidth(100)
+                # -- add to layout & return
+                elem.addWidget(self.dosage_statement)
+                elem.addWidget(self.btn_validate)
+                return elem
+
+            def r_btns():
+                elem = QHBoxLayout()
+                self.btn_reset = QPushButton("Reset")
+                self.btn_submit = QPushButton("Submit")
+                elem.addWidget(self.btn_reset)
+                elem.addWidget(self.btn_submit)
+                return elem
+            # --- layout
             elem = QVBoxLayout()
+            # --- title
             lbl_title = QLabel("Edit Medication")
             elem.addWidget(lbl_title)
+            elem.addLayout(r_med_name())
+            elem.addLayout(r_str_qty())
+            elem.addLayout(r_input_dosage())
+            elem.addLayout(r_display_dosage())
+            elem.addLayout(r_btns())
             return elem
 
-        def t_med_table():
+        def build_t_med_table():
             elem = QVBoxLayout()
             # --- title row
             lbl_title = QLabel("Ready to check in")
@@ -79,10 +248,10 @@ class MedsInTool(QWidget):
             row.addLayout(btn_panel)
             return row
 
-        def r_tools_0():
+        def build_r_tools_0():
             elem = QHBoxLayout()
-            elem.addLayout(t_med_editor())
-            elem.addLayout(t_med_table())
+            elem.addLayout(build_t_med_editor())
+            elem.addLayout(build_t_med_table())
             return elem
 
         # instantiate overall layout
@@ -90,8 +259,8 @@ class MedsInTool(QWidget):
         self.setLayout(wrapper)
 
         # add r elements to wrapper
-        wrapper.addLayout(r_header())
-        wrapper.addLayout(r_tools_0())
+        wrapper.addLayout(build_r_header())
+        wrapper.addLayout(build_r_tools_0())
 
 
 # APP
